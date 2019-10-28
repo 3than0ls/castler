162c764e2a9fe6db11fb4a25e35e4fa39244be19
Ethan <ethanolchennault@gmail.com>
2019-10-28 15:16:56 -0700

    added server and webpack files, and a simple game file

--- server/server.js
+++ server/server.js
@@ -1,2 +1,18 @@
-// server side code here - socket io
-// added some more code!
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware')
+
+const app = express();
+const config = require('./../../webpack.config.js');
+const compiler = webpack(config);
+
+app.use(express.static(path.join(__dirname, './src/')));
+// include public static files later
+
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+}));
+
+  
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/game.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]   
+  }
+};
